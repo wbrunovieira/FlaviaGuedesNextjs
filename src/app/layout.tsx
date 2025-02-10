f77661df8a181acd99841c13a78bbf54d68eddc5
Bruno MacM3 <wbrunovieira77@gmail.com>
2025-02-10 09:12:12 -0300

    added fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,24 @@
 // src/app/layout.tsx
 import './globals.css';
 import type { Metadata } from 'next';
+import { Merriweather, Work_Sans } from 'next/font/google';
 
 export const metadata: Metadata = {
   title: 'Flavia Guedes',
   description: '',
 };
+
+const merriweather = Merriweather({
+  subsets: ['latin'],
+  weight: ['300', '400', '700'],
+  variable: '--font-merriweather',
+});
+
+const workSans = Work_Sans({
+  subsets: ['latin'],
+  weight: ['300', '400', '700'],
+  variable: '--font-work-sans',
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +26,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html
+      lang="pt-BR"
+      className={`${merriweather.variable} ${workSans.variable}`}
+    >
       <head>
         <title>Flavia Guedes</title>
       </head>
